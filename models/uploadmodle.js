8e72d7747cc71c56914fc92701fcf60d7b34e560
AnupamMY <119336682+AnupamMY@users.noreply.github.com>
2024-10-02 14:01:15 +0530

    Add files via upload

---
+++ models/uploadmodle.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const uploadschema = new mongoose.Schema({
+    photo:{
+        type:String,
+        required:true,
+    },
+    subject:{
+        type:String,
+        required:true,
+    },
+    
+
+    
+
+},{timestamps:true});
+
+const Upload = mongoose.model('photos',uploadschema)
+
+module.exports = Upload
---
+++ routes/UploadRoute.js
@@ -0,0 +1,42 @@
+const {Router} = require('express')
+const UploadMiddleware = require('../middleware/multermiddleware');
+const uploadmodle = require('../models/uploadmodle');
+
+const router = Router()
+
+router.post('/api/save',UploadMiddleware.single('photo'),(req,res)=>{
+    const photo = req.file.filename
+    const subject= req.body.subject
+    uploadmodle.create({
+        photo,subject
+    }).then((data)=>{
+        console.log('upload success');
+        console.log(data);
+        res.send(data)
+    }).catch((e)=>{
+        console.log('error',e);
+    })
+})
+
+router.get('/api/get', async (req,res)=>{
+    const allPhotos = await uploadmodle.find().sort({
+        createAt:'descending'
+    })
+    res.send(allPhotos)
+})
+
+
+router.post('/api/search', async (req,res)=>{
+    const subject= req.body.subject
+    
+    const allPhotos = subject=='all'? await uploadmodle.find().sort({
+        createAt:'descending'
+    }) : await uploadmodle.find({subject}).sort({
+        createAt:'descending'
+    })
+    console.log(allPhotos);
+    res.send(allPhotos)
+})
+
+module.exports=router
+
